53a1527a3aefe53f4c9608b73c801a9ce7439aef
binaryArrow <cancihan96@gmail.com>
2023-01-12 19:41:13 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+git init
+git checkout -B master
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:binaryArrow/gclogvisualizer.git master:gh-pages
+
+cd -
